f3ec609f9b0f7705cac3de4988e12dfbf91af66d
margin-right <i.ruden@mail.ru>
2021-11-18 21:38:15 +1000

    OOO fix

--- pages/about/index.js
+++ pages/about/index.js
@@ -7,7 +7,7 @@ export default function About(){
             <Head>
                 <title>Эбаут</title>
                 <link rel="icon" href="/favicon.ico" />
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"></link>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
             </Head>
             <div className="container">
                 <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
