495a1e71ff6700f9e2e85412a0408aefa5f23c86
margin-right <i.ruden@mail.ru>
2021-11-18 21:19:14 +1000

    little fix

--- pages/posts.js
+++ pages/posts.js
@@ -7,14 +7,14 @@ export default function Posts(){
             <Head>
                 <title>Пастыы</title>
                 <link rel="icon" href="/favicon.ico" />
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"></link>
             </Head>
             <div className="container">
                 <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <Link href={'/'}><a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                     
                     <span className="fs-4">Посты, какие посты блин</span>
-                </a>
+                </a></Link>
 
 
 
