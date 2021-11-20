c0587ad02d77e0609e0137976ec8d79d57321aa3
margin-right <i.ruden@mail.ru>
2021-11-20 22:04:38 +1000

    9999 try to fix deploy

---
+++ components/Form.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { loginUser } from "../utils/auth";
+
+export default function Form() {
+
+    const [state, setState] = useState({login: '',password: ''})
+    const [loginState, setLogin] = useState('');
+    const [passwordState, setPassword] = useState('');
+
+
+    return (
+
+        
+
+        <form className="p-4 p-md-5 border rounded-3 bg-light" onSubmit={event => {event.preventDefault();console.log(state);loginUser(loginState, passwordState);}}>
+            <div className="form-floating mb-3">
+                <input type="text" className="form-control" id="floatingInput" name="login" placeholder="name@example.com" onChange={event => setLogin(event.target.value)}></input>
+                <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating mb-3">
+                <input type="password" className="form-control" id="floatingPassword" name="password" placeholder="Password" onChange={event => setPassword(event.target.value)}></input>
+                <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <div className="checkbox mb-3">
+                <label>
+                <input type="checkbox" value="remember-me"></input> Remember me
+                </label>
+            </div>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+            <hr className="my-4"></hr>
+            <small className="text-muted">By clicking Sign up, you agree to the terms of use.</small>
+            </form>
+    )
+}
