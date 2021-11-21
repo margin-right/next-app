f336ed7d6e654295d6bc0ea845ffeb366aab3eaf
margin-right <i.ruden@mail.ru>
2021-11-22 01:56:35 +1000

    nevermind

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({users}) {
   }
 
     async function dataUpdate() {
-      const ress = await fetch('http://localhost:3000/api/users');
+      const ress = await fetch('https://next-app-tau-lake.vercel.app/api/users');
       const NewData = await ress.json();
       console.log(NewData.data)
       setUssers(NewData.data)
@@ -67,7 +67,7 @@ export default function Home({users}) {
 }
 
 Home.getInitialProps = async () =>{
-  const res = await fetch('http://localhost:3000/api/users');
+  const res = await fetch('https://next-app-tau-lake.vercel.app/api/users');
   const {data} = await res.json();
   return {users: data}
 } 
